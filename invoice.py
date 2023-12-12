a56b537e07458f45251e9d848eeb2e2e557a72f3
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-12-11 22:23:56 -0600

    finish generate function and make it package compatible (initialize arguments and make them dynamic)

--- invoice.py
+++ invoice.py
@@ -1,11 +1,12 @@
+import os
 import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
 
 
-def generate():
-    filepaths = glob.glob("invoices/*.xlsx")
+def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amount_purchased, price_per_unit, total_price):
+    filepaths = glob.glob(f"{invoices_path}/*.xlsx")
 
     for filepath in filepaths:
 
@@ -38,13 +39,13 @@ def generate():
         for index, row in df.iterrows():
             pdf.set_font(family="Times", size=10)
             pdf.set_text_color(80, 80, 80)
-            pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-            pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
-            pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
-            pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
-            pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+            pdf.cell(w=30, h=8, txt=str(row[product_id]), border=1)
+            pdf.cell(w=70, h=8, txt=str(row[product_name]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[amount_purchased]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[price_per_unit]), border=1)
+            pdf.cell(w=30, h=8, txt=str(row[total_price]), border=1, ln=1)
 
-        total_sum = df["total_price"].sum()
+        total_sum = df[total_price].sum()
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt="", border=1)
@@ -60,7 +61,7 @@ def generate():
         # Add company name and logo
         pdf.set_font(family="Times", size=14, style="B")
         pdf.cell(w=25, h=8, txt=f"PythonHow")
-        pdf.image("pythonhow.png", w=10)
+        pdf.image(image_path, w=10)
 
-
-        pdf.output(f"PDFs/{filename}.pdf")
+        os.makedirs(pdfs_path)
+        pdf.output(f"{pdfs_path}/{filename}.pdf")

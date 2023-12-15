121505700701c274f48e96e4033d8a0ae9d68ceb
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-12-14 23:01:17 -0600

    initialize setup for pypi, update invoice.py throughput

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import invoice
+from invoicing import invoice
 
 invoice.generate("invoices", "output", "pythonhow.png", "product_id", "product_name",
                  "amount_purchased", "price_per_unit", "total_price")
--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 
 setup(
-  name = 'invoicing',         #* Your package will have this name
+  name = 'pdf-invoice',         #* Your package will have this name
   packages = ['invoicing'],   #* Name the package again
   version = '1.0.0',         #* To be increased every time your change your library
   license='MIT',             # Type of license. More here: https://help.github.com/articles/licensing-a-repository
-  description = 'This package can be used to convert Excel invoices to PDF invoices.',    # Short description of your library
-  author = 'Ardit Sulce',                   # Your name
-  author_email = 'your.email@example.com',  # Your email
-  url = 'https://example.com',              # Homepage of your library (e.g. github or your website)
+  description = 'This package is used to convert Excel to PDF invoices.',    # Short description of your library
+  author = 'Raphael Evangelista',                   # Your name
+  author_email = 'raphaelsebastien.evangelista@gmail.com',  # Your email
+  url = 'https://github.com/skythepoppy/pythonPackage',        # Homepage of your library (e.g. github or your website)
   keywords = ['invoice', 'excel', 'pdf'],   # Keywords users can search on pypi.org
-  install_requires=['fpdf', 'openpyxl'],                 # Other 3rd-party libs that pip needs to install
+  install_requires=['pandas', 'fpdf', 'openpyxl'],                 # Other 3rd-party libs that pip needs to install
   classifiers=[
     'Development Status :: 3 - Alpha',          # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',          # Who is the audience for your library?
